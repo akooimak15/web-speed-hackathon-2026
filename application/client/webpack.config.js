2916d6ada3ed576cc66157bc9993a62c2ca6182c
akooimak15 <mthunder103@gmail.com>
2026-03-20 12:49:39 +0900

    Revert "fix: webpack production mode"
    This reverts commit bc8570c04d3bc4085818f5b40805011291f29f20.

--- application/client/webpack.config.js
+++ application/client/webpack.config.js
@@ -17,18 +17,26 @@ const config = {
     historyApiFallback: true,
     host: "0.0.0.0",
     port: 8080,
-    proxy: [{ context: ["/api"], target: "http://localhost:3000" }],
+    proxy: [
+      {
+        context: ["/api"],
+        target: "http://localhost:3000",
+      },
+    ],
     static: [PUBLIC_PATH, UPLOAD_PATH],
   },
-  devtool: false,
+  devtool: "inline-source-map",
   entry: {
     main: [
+      "core-js",
+      "regenerator-runtime/runtime",
+      "jquery-binarytransport",
       path.resolve(SRC_PATH, "./index.css"),
       path.resolve(SRC_PATH, "./buildinfo.ts"),
       path.resolve(SRC_PATH, "./index.tsx"),
     ],
   },
-  mode: "production",
+  mode: "none",
   module: {
     rules: [
       {
@@ -52,23 +60,27 @@ const config = {
   },
   output: {
     chunkFilename: "scripts/chunk-[contenthash].js",
-    filename: "scripts/[name].[contenthash].js",
+    chunkFormat: false,
+    filename: "scripts/[name].js",
     path: DIST_PATH,
     publicPath: "auto",
     clean: true,
   },
   plugins: [
     new webpack.ProvidePlugin({
+      $: "jquery",
       AudioContext: ["standardized-audio-context", "AudioContext"],
       Buffer: ["buffer", "Buffer"],
+      "window.jQuery": "jquery",
     }),
     new webpack.EnvironmentPlugin({
       BUILD_DATE: new Date().toISOString(),
+      // Heroku では SOURCE_VERSION 環境変数から commit hash を参照できます
       COMMIT_HASH: process.env.SOURCE_VERSION || "",
-      NODE_ENV: "production",
+      NODE_ENV: "development",
     }),
     new MiniCssExtractPlugin({
-      filename: "styles/[name].[contenthash].css",
+      filename: "styles/[name].css",
     }),
     new CopyWebpackPlugin({
       patterns: [
@@ -88,6 +100,26 @@ const config = {
     alias: {
       "bayesian-bm25$": path.resolve(__dirname, "node_modules", "bayesian-bm25/dist/index.js"),
       ["kuromoji$"]: path.resolve(__dirname, "node_modules", "kuromoji/build/kuromoji.js"),
+      "@ffmpeg/ffmpeg$": path.resolve(
+        __dirname,
+        "node_modules",
+        "@ffmpeg/ffmpeg/dist/esm/index.js",
+      ),
+      "@ffmpeg/core$": path.resolve(
+        __dirname,
+        "node_modules",
+        "@ffmpeg/core/dist/umd/ffmpeg-core.js",
+      ),
+      "@ffmpeg/core/wasm$": path.resolve(
+        __dirname,
+        "node_modules",
+        "@ffmpeg/core/dist/umd/ffmpeg-core.wasm",
+      ),
+      "@imagemagick/magick-wasm/magick.wasm$": path.resolve(
+        __dirname,
+        "node_modules",
+        "@imagemagick/magick-wasm/dist/magick.wasm",
+      ),
     },
     fallback: {
       fs: false,
@@ -96,14 +128,20 @@ const config = {
     },
   },
   optimization: {
-    minimize: true,
-    splitChunks: { chunks: "all" },
-    concatenateModules: true,
-    usedExports: true,
-    providedExports: true,
-    sideEffects: true,
+    minimize: false,
+    splitChunks: false,
+    concatenateModules: false,
+    usedExports: false,
+    providedExports: false,
+    sideEffects: false,
   },
-  cache: true,
+  cache: false,
+  ignoreWarnings: [
+    {
+      module: /@ffmpeg/,
+      message: /Critical dependency: the request of a dependency is an expression/,
+    },
+  ],
 };
 
 module.exports = config;
