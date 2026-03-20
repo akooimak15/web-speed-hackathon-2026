9eb5538554b3707a3744d615071106d1c0c96465
akooimak15 <mthunder103@gmail.com>
2026-03-20 12:49:39 +0900

    Revert "fix4"
    This reverts commit 6a231a3857c0bba866d42a9d4fcc648a257d5f2a.

--- application/client/webpack.config.js
+++ application/client/webpack.config.js
@@ -52,7 +52,7 @@ const config = {
   },
   output: {
     chunkFilename: "scripts/chunk-[contenthash].js",
-    filename: "scripts/[name].js",
+    filename: "scripts/[name].[contenthash].js",
     path: DIST_PATH,
     publicPath: "auto",
     clean: true,
@@ -68,7 +68,7 @@ const config = {
       NODE_ENV: "production",
     }),
     new MiniCssExtractPlugin({
-      filename: "styles/[name].css",
+      filename: "styles/[name].[contenthash].css",
     }),
     new CopyWebpackPlugin({
       patterns: [
