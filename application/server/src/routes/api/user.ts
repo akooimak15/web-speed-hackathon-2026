c08c8bef79c28002c3b1e12cb3e2da35e76ef55f
akooimak15 <mthunder103@gmail.com>
2026-03-20 17:23:41 +0900

    fix: optimize user posts query, reduce N+1

--- application/server/src/routes/api/user.ts
+++ application/server/src/routes/api/user.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import httpErrors from "http-errors";
-
 import { Post, User } from "@web-speed-hackathon-2026/server/src/models";
 
 export const userRouter = Router();
@@ -10,11 +9,9 @@ userRouter.get("/me", async (req, res) => {
     throw new httpErrors.Unauthorized();
   }
   const user = await User.findByPk(req.session.userId);
-
   if (user === null) {
     throw new httpErrors.NotFound();
   }
-
   return res.status(200).type("application/json").send(user);
 });
 
@@ -23,49 +20,36 @@ userRouter.put("/me", async (req, res) => {
     throw new httpErrors.Unauthorized();
   }
   const user = await User.findByPk(req.session.userId);
-
   if (user === null) {
     throw new httpErrors.NotFound();
   }
-
   Object.assign(user, req.body);
   await user.save();
-
   return res.status(200).type("application/json").send(user);
 });
 
 userRouter.get("/users/:username", async (req, res) => {
   const user = await User.findOne({
-    where: {
-      username: req.params.username,
-    },
+    where: { username: req.params.username },
   });
-
   if (user === null) {
     throw new httpErrors.NotFound();
   }
-
   return res.status(200).type("application/json").send(user);
 });
 
 userRouter.get("/users/:username/posts", async (req, res) => {
   const user = await User.findOne({
-    where: {
-      username: req.params.username,
-    },
+    where: { username: req.params.username },
+    attributes: ["id"],
   });
-
   if (user === null) {
     throw new httpErrors.NotFound();
   }
-
   const posts = await Post.findAll({
     limit: req.query["limit"] != null ? Number(req.query["limit"]) : undefined,
     offset: req.query["offset"] != null ? Number(req.query["offset"]) : undefined,
-    where: {
-      userId: user.id,
-    },
+    where: { userId: user.id },
   });
-
   return res.status(200).type("application/json").send(posts);
 });
