e2cca436b6696dacec3a93b02171c29735a514d1
akooimak15 <mthunder103@gmail.com>
2026-03-21 17:43:18 +0900

    fix: remove jquery from webpack bundle

--- application/client/webpack.config.js
+++ application/client/webpack.config.js
@@ -30,7 +30,6 @@ const config = {
     main: [
       "core-js",
       "regenerator-runtime/runtime",
-      "jquery-binarytransport",
       path.resolve(SRC_PATH, "./index.css"),
       path.resolve(SRC_PATH, "./buildinfo.ts"),
       path.resolve(SRC_PATH, "./index.tsx"),
@@ -68,9 +67,7 @@ const config = {
   },
   plugins: [
     new webpack.ProvidePlugin({
-      $: "jquery",
       Buffer: ["buffer", "Buffer"],
-      "window.jQuery": "jquery",
     }),
     new webpack.EnvironmentPlugin({
       BUILD_DATE: new Date().toISOString(),
