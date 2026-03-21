f47fbca01faa395ed942d3a15fa4c8e33158d019
akooimak15 <mthunder103@gmail.com>
2026-03-21 11:11:30 +0900

    fix: remove standardized-audio-context, use native AudioContext

--- application/client/webpack.config.js
+++ application/client/webpack.config.js
@@ -69,7 +69,6 @@ const config = {
   plugins: [
     new webpack.ProvidePlugin({
       $: "jquery",
-      AudioContext: ["standardized-audio-context", "AudioContext"],
       Buffer: ["buffer", "Buffer"],
       "window.jQuery": "jquery",
     }),
